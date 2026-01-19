ecfdfd9949b6444f363da68cb2247c2fea7f38bc
Лесогоров Станислав <staslesogorov@gmail.com>
2026-01-19 18:34:24 +0300

    сделал ограничения на уровне sql

--- Vending.Api/Data/AppDbContext.cs
+++ Vending.Api/Data/AppDbContext.cs
@@ -17,8 +17,60 @@ public class AppDbContext : DbContext
     {
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<User>(e =>
-            e.ToTable(t => t.HasCheckConstraint("checkUserFio", "\"FIO\" != 'test'"))
-        );
+        modelBuilder.Entity<VendingMachine>(entity =>
+        {
+            entity.HasIndex(e => e.SerialNumber)
+                .IsUnique()
+                .HasDatabaseName("uniq_vendingmachine_serial_number");
+
+            entity.HasIndex(e => e.InventoryNumber)
+                .IsUnique()
+                .HasDatabaseName("uniq_vendingmachine_inventory_number");
+
+
+            entity.ToTable(t =>
+            {
+                t.HasCheckConstraint(
+                    "check_total_revenue_positive",
+                    "\"TotalRevenue\" >= 0"
+                );
+
+                t.HasCheckConstraint(
+                    "check_resource_hours_positive",
+                    "\"ResourceHours\" > 0"
+                );
+
+                t.HasCheckConstraint(
+                    "check_maintenance_duration_range",
+                    "\"MaintenanceDurationHours\" BETWEEN 1 AND 20"
+                );
+
+                t.HasCheckConstraint(
+                    "check_calibration_interval_positive",
+                    "\"CalibrationIntervalMonths\" > 0"
+                );
+
+                t.HasCheckConstraint(
+                    "check_commissioning_date",
+                    "\"CommissioningDate\" >= \"ManufactureDate\" AND \"CommissioningDate\" <= CURRENT_DATE"
+                );
+
+                t.HasCheckConstraint(
+                    "check_last_calibration_date",
+                    "\"LastCalibrationDate\" >= \"ManufactureDate\" AND \"LastCalibrationDate\" <= CURRENT_DATE"
+                );
+
+                t.HasCheckConstraint(
+                    "check_inventory_date",
+                    "\"InventoryDate\" >= \"ManufactureDate\" AND \"InventoryDate\" <= CURRENT_DATE"
+                );
+
+                t.HasCheckConstraint(
+                    "check_next_maintenance_date",
+                    "\"NextMaintenanceDate\" > CURRENT_DATE"
+                );
+            });
+        });
     }
+
 }
